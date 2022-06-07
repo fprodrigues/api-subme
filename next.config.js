ecbe910442b3af96b293b2821727b39d4e34a318
fprodrigues <dombalder@gmail.com>
2022-06-07 19:32:24 -0300

    proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://https://ec2-54-83-81-178.compute-1.amazonaws.com/:path*',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
